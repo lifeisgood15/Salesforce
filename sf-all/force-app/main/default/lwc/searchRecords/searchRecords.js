3af9b7ca3054057ed5c1fae1865684d8e4fddba4
Shaily Pandey <shailypandey414@gmail.com>
2024-02-20 15:44:48 +0530

    Dispatch event on Account select

--- sf-all/force-app/main/default/lwc/searchRecords/searchRecords.js
+++ sf-all/force-app/main/default/lwc/searchRecords/searchRecords.js
@@ -1,15 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class SearchRecords extends LightningElement {
-    matchingInfo = {
-        primaryField: { fieldPath: 'Name' },
-        additionalFields: [{ fieldPath: 'Owner.Name' }],
-    };
-    displayInfo = {
-        primaryField: 'Account.Name',
-        additionalFields: ['Account.Phone'],
-    };
-    handleOnChange(e) {
-        console.debug('Selected record', e.detail.recordId)
-    }
+  @api apiName;
+  @api listViewApiName;
+  matchingInfo = {
+    primaryField: { fieldPath: "Name" },
+    additionalFields: [{ fieldPath: "Owner.Name" }]
+  };
+  displayInfo = {
+    primaryField: "Name",
+    additionalFields: ["Phone"]
+  };
+  handleOnChange(e) {
+    console.debug("Selected record", e.detail.recordId);
+    this.dispatchEvent(
+      new CustomEvent("itemselected", {
+        detail: { recordId: e.detail.recordId, apiName: this.apiName }
+      })
+    );
+  }
 }
