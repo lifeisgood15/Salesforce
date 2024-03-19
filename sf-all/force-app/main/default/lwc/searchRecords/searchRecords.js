060c67c2056f85a9edeeac45ce3e3f0d20ffab90
Shaily Pandey <shailypandey414@gmail.com>
2024-03-19 12:44:49 +0530

    Search record changes

--- sf-all/force-app/main/default/lwc/searchRecords/searchRecords.js
+++ sf-all/force-app/main/default/lwc/searchRecords/searchRecords.js
@@ -3,6 +3,27 @@ import { LightningElement, api } from "lwc";
 export default class SearchRecords extends LightningElement {
   @api apiName;
   @api listViewApiName;
+
+  selectedRecord = "";
+  selectedObjects = [];
+  availableObjects = [
+    {
+      Label: "Financial Accounts",
+      ApiName: "FinServ__FinancialAccount__c"
+    },
+    {
+      Label: "Opportunities",
+      ApiName: "Opportunity"
+    },
+    {
+      Label: "Leads",
+      ApiName: "Lead"
+    },
+    {
+      Label: "Contacts",
+      ApiName: "Contact"
+    }
+  ];
   matchingInfo = {
     primaryField: { fieldPath: "Name" },
     additionalFields: [{ fieldPath: "Owner.Name" }]
@@ -12,10 +33,25 @@ export default class SearchRecords extends LightningElement {
     additionalFields: ["Phone"]
   };
   handleOnChange(e) {
-    console.debug("Selected record", e.detail.recordId);
+    this.selectedRecord = e.detail.recordId;
+    this.dispatchChange();
+  }
+
+  handleObjectChange(e) {
+    if (e.target.checked) {
+      this.selectedObjects.push(e.target.value);
+    } else {
+      this.selectedObjects = this.selectedObjects.filter(
+        (obj) => obj !== e.target.value
+      );
+    }
+    this.dispatchChange();
+  }
+
+  dispatchChange() {
     this.dispatchEvent(
       new CustomEvent("itemselected", {
-        detail: { recordId: e.detail.recordId, apiName: this.apiName }
+        detail: { recordId: this.selectedRecord, apiName: this.selectedObjects }
       })
     );
   }
